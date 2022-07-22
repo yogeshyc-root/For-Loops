ff0affa0be80d81dbbce6770166f54f704b4ac55
Yogeshwaran C <84272111+ycyogesh@users.noreply.github.com>
2022-07-22 23:02:46 +0530

    Create Finding Substring in a String.py
    Finding Substring in a String using 2 for loops. It is slighty complicated to understand but you will clearly undestand once you put the state table.

---
+++ Finding Substring in a String.py
@@ -0,0 +1,25 @@
+a = input("Enter String :")
+b = input("Enter Finding Substring or Letter:")
+
+flag = False
+
+count = 0
+
+temp = []
+
+for i in range(0,len(a)):
+    if(b[0]==a[i]):
+        for j in range(0,len(b)):
+            if(a[i]==b[j]) and (i+len(b)<len(b)):
+                flag = True
+                temp.append(a[i])
+                i+=1
+            else:
+                flag = False
+                break
+        if(flag):
+            count+=1
+    
+if (not flag):
+    print("Not Found")
+print(temp,count)
