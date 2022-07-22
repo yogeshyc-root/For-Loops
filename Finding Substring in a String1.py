ef0694625159752e715f96654f70557cc56c6560
Yogeshwaran C <84272111+ycyogesh@users.noreply.github.com>
2022-07-22 23:09:00 +0530

    Create Finding Substring in a String1.py
    Finding Substring in a String using single for loop.

---
+++ Finding Substring in a String1.py
@@ -0,0 +1,17 @@
+a = input("Enter String :")
+b = input("Enter Finding Substring or Letter:")
+
+flag = False
+
+count = 0
+j = 0
+
+for i in range(0,len(a)):
+    if(a[i]==b[j]):
+        j+=1
+    else:
+        j=0
+    if(j == len(b)):
+        count+=1
+        j = 0
+print(count)
