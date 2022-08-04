744e5d1565d35ad5b7fe41ef51d19e04524783b1
ycyogesh <ycyogesh183@gmail.com>
2022-08-04 17:58:28 +0530

    Added some lines

--- Binary to Octal.py
+++ Binary to Octal.py
@@ -8,3 +8,14 @@ n1 = len(n)-1
 for i in range(n1,-1,-1):
     sum = int(n[int(i)]) * (2**int(j)) + sum
     j+=1
+
+new = sum
+rem = 0
+a = []
+for i in range(0,len(str(new))):
+    rem = new%8
+    new//=16
+    print(new)
+    a.append(rem)
+for i in range(len(a)-1,-1,-1):
+    print(a[i],end="")
