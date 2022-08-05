317600e5c9932a797ec79538146d2adbcefd2564
ycyogesh <ycyogesh183@gmail.com>
2022-08-05 23:06:02 +0530

    Checkboard Pattern

--- Checkboard pattern.py
+++ Checkboard pattern.py
@@ -8,7 +8,9 @@ t = "black"
 
 for i in range(0,n):
     for j in range(0,n):
-        print(t,end="-")
+        print(t,end='')
+        if(j!=n-1):
+            print(end="-")
         t=w
         w=b
         b=t
