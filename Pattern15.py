161b70189862166a057551076f55db3874549f39
ycyogesh <ycyogesh183@gmail.com>
2022-08-05 23:16:43 +0530

    Power of 2 Pattern

---
+++ Pattern15.py
@@ -0,0 +1,16 @@
+#Power of 2 Pattern
+
+n = int(input("Enter Number : "))
+temp = [1]
+temp1=[]
+print(n*' ',temp)
+for j in range(0,n):
+    for i in range(j,n):
+        print(" ",end='')
+    temp1.append(1)
+    for i in range(1,len(temp)):
+        temp1.append(2**i)
+    temp1.append(1)
+    print(temp1)
+    temp = temp1
+    temp1 = []
