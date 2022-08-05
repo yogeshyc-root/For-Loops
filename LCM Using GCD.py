595ef5773c8f132d804aa110ffb73ad1f13da15a
ycyogesh <ycyogesh183@gmail.com>
2022-08-05 08:02:05 +0530

    LCM of 2 Numbers

--- LCM Using GCD.py
+++ LCM Using GCD.py
@@ -1,16 +1,21 @@
-# LCM of 2 Numbers
+# LCM of 2 Numbers using HCF
+
+def gcde(a,b):
+    if(a>b):
+        min = b
+    else:
+        min = a
+
+    for i in range(min,0,-1):
+        if(a%i==0 and b%i==0):
+            gcd = i
+            return gcd
 
 n1 = int(input("Enter Number 1 : "))
 n2 = int(input("Enter Number 2 : "))
 gcd = 0
+gcd = gcde(n1,n2)
 
-if(n1>n2):
-    min = n2
-else:
-    min = n1
-
-for i in range(min,0,-1):
-    if(n1%i==0 and n2%i==0):
-        gcd = i
-        break
-print(gcd)
+lcm = 0
+lcm = (n1*n2)/gcd
+print(lcm)
