caa1f828cebbf25c3b1e07f76b125db2ad95f513
ycyogesh <ycyogesh183@gmail.com>
2022-08-07 21:46:07 +0530

    Some changes added

--- Pattern15.py
+++ Pattern15.py
@@ -10,7 +10,11 @@ for j in range(0,n):
     temp1.append(1)
     for i in range(1,len(temp)):
         temp1.append(2**i)
-    temp1.append(1)
+    # temp1.append(1)
+    for i in range(len(temp)-1,-1,-1):
+        temp1.append(temp1[i-1])
+        if(len(temp1)!=len(temp)):
+            break
     print(temp1)
     temp = temp1
     temp1 = []
