9a39e3e26f1719b7bd3fdffbf8fc6a8014ed4bea
ycyogesh <ycyogesh183@gmail.com>
2022-08-09 23:25:25 +0530

    Pyramid of Digits

--- Pattern19.py
+++ Pattern19.py
@@ -4,12 +4,15 @@
 n = 5
 a = 1
 for i in range(0,n):
-    a = 1
     for j in range(i,n):
         print(" ",end="")
+        a = i+1
     for j in range(i+1):
         print(a,end="")
         a+=1
+        b = a-2
     for j in range(j,0,-1):
-        print(j,end="")
+        print(b,end="")
+        b-=1
     print()
+    
