7a1fd2387b92bc9508452c9c0a433ef7aa8e746e
ycyogesh <ycyogesh183@gmail.com>
2022-08-09 10:04:35 +0530

    Changes

--- Pattern16.py
+++ Pattern16.py
@@ -1,6 +1,7 @@
 # Pattern Matrix
 
-n = int(input("Enter Number : "))
+# n = int(input("Enter Number : "))
+n = 5
 a = 0
 b = -1
 for i in range(0,n):
@@ -8,7 +9,10 @@ for i in range(0,n):
     for j in range(0,n):
         print(a,end="")
         a+=1
+        if(a==i):
+            a=0
         if(a==n):
             a=0
+
     print()
     b+=1
