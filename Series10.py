869d7641afeaed8b34f602169e2e55132339a232
ycyogesh <ycyogesh183@gmail.com>
2022-08-10 08:02:54 +0530

    Changes added

--- Series10.py
+++ Series10.py
@@ -1,1 +1,18 @@
-# Sum of Series 
+# Sum of Series x - x^3 + x^5 + ......
+
+x = int(input("Enter X Value : "))
+n = int(input("Enter Limit : "))
+
+sum = x
+a = 3
+
+for i in range(1,n+1):
+    if(i%2==0):
+        sum = sum - (x**a)
+        print(sum," ",a)
+        a+=2
+    else:
+        sum = sum + (x**a)
+        print(sum," ",a)
+        a+=2
+print(sum)
