976f78be66f7ad8466571a413eae94ffd0c002fe
ycyogesh <ycyogesh183@gmail.com>
2022-08-10 08:44:19 +0530

    Pattern Pyramid Alphbaet

--- Pattern21.py
+++ Pattern21.py
@@ -3,3 +3,17 @@ a = []
 for i in range(65,91):
     a.append(chr(i))
 
+# n = int(input("Enter Number of Letters : "))
+n = 5
+k = 0
+for i in range(0,n):
+    for j in range(i,n):
+        print(" ",end = " ")
+        k = 0
+    for j in range(i):
+        print(a[k],end=" ")
+        k +=1
+    for j in range(i+1):
+        print(a[k],end = " ")
+        k-=1
+    print()
